5eadfe3fa62f8a30cbba50d7c02638c06e296f04
rakibulhasan65 <rakibulhasan392364@gmail.com>
2023-05-03 09:53:11 +0600

    Laravel Vue js And Tailwindcss Install Done

---
+++ My-Professional-Website/resources/js/component/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h4 class="bg-gray-200 text-center">Hello Bangladesh</h4>
+    </div>
+</template>
---
+++ My-Professional-Website/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body>
+        <div id="app">
+        </div>
+
+        <div class="text-lg bg-slate-500 text-center" >Hello Laravel Vue Js And Tailwindcss</div>
+    </body>
+</html>
